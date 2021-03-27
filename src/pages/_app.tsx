070292c6dc26a2b129f64a40d1b7d0f97147ea0d
caiohenrique-developer <caiorussellrox@msn.com>
2021-03-27 04:55:29 -0300

    :chart_with_upwards_trend: [ WIP ]: Add handle theme mode with success

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
-import GlobalStyle from '../styles/globals'
+import { useState } from 'react';
+import GlobalStyle, { themeMode } from '../styles/globals'
+import { ThemeProvider } from 'styled-components'
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState('dark');
 
+  const handleThemeModeToggle = () => { theme === 'dark' ? setTheme('light') : setTheme('dark'); }
+  
   return (
     <>
-      <Component {...pageProps} />
-      <GlobalStyle />
+      <ThemeProvider theme={ theme === 'dark' ? themeMode.dark : themeMode.light }>
+        <Component {...pageProps} />
+
+        <button
+          onClick={handleThemeModeToggle}
+        >Alterar tema</button>
+
+        <GlobalStyle />
+      </ThemeProvider>
     </>
   )
 }

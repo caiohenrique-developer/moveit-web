2d38d54439345054bb9a138cc29e259dfc929554
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-03 23:56:31 -0300

    :art: [ STYLE ]: Add avatar profile handler style

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,33 @@
-import { useContext } from 'react';
+import { ChangeEvent, useContext, useState } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     const { level } = useContext(ChallengesContext);
 
+    const [avatar, setAvatar] = useState('');
+
+    const handleProfileAvatar = (ev: ChangeEvent<HTMLInputElement>) => {
+        console.log('Aqui vem o nome do arquivo.');
+        setAvatar(ev.target.files[0].name);
+        console.log(avatar);
+    }
+
     return (
         <div className={styles.profileContainer}>
-            <img src="github-profile-avatar.jpeg" alt="GitHub profile avatar" />
+            <div className={styles.profileAvatar}>
+                <img src={"github-profile-avatar.jpeg"} alt="GitHub profile avatar" />
+                <label htmlFor="avatar">
+                    <input
+                        type="file"
+                        id="avatar"
+                        placeholder="Alterar foto"
+                        capture
+                        onChange={handleProfileAvatar}
+                    />
+                </label>
+            </div>
+            
             <div>
                 <strong>Caio Henrique</strong>
                 <p>

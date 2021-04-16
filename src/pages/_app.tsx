639e340334e10f7c530b07cb20b495aad9eb4b85
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-16 17:20:25 -0300

    :construction: [ WIP ]: Correcting definition props from _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import { AppProps } from 'next/app';
 
 import { ThemeModeProvider } from '@hooks/useThemeMode';
 
 import GlobalStyle from '@styles/globals';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ThemeModeProvider>
       <Component {...pageProps} />

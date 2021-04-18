a7a3401c20c6555c15aa38ef29bd99091af9434a
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-18 17:57:22 -0300

    :rocket: [ PERF ]: Add `Modal.setAppElement('#__next');` to correct use with React modal lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 import { AppProps } from 'next/app';
 
 import { ThemeModeProvider } from '@hooks/useThemeMode';
 
 import GlobalStyle from '@styles/globals';
+
+Modal.setAppElement('#__next');
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
